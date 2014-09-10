676e7393fb45de7bf513436e4d9aac1a6ea163c0
Mario Carrasquero <mncarrasquero@gmail.com>
2014-09-09 23:42:48 -0430

    quitando moment por los momentos

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,7 +8,7 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
-var moment = require('moment');
+
 module.exports.bootstrap = function(cb) {
 
 	setInterval(function() {
