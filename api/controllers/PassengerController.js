76b634f75a91ecf758938bccb592d78febbcc58b
Mario Carrasquero <mncarrasquero@gmail.com>
2014-11-29 17:20:14 -0430

    asdasd

--- api/controllers/PassengerController.js
+++ api/controllers/PassengerController.js
@@ -189,7 +189,7 @@ module.exports = {
 
 					console.log(beginningTime.toDate());
 					console.log(endTime.toDate());
-					console.log(moment().zone(data[0].timeZone));
+					console.log(moment().zone(data[0].timeZone).toDate());
 					console.log(now.toDate());
 					console.log(data[0].timeZone);
 
