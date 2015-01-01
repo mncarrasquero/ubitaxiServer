d7c930e0e756aa8911cb171392f2ea47a470f597
Mario Carrasquero <mncarrasquero@gmail.com>
2015-01-01 18:30:03 +0000

    test new relic

--- newrelic.js
+++ newrelic.js
@@ -1,24 +1,9 @@
-/**
- * New Relic agent configuration.
- *
- * See lib/config.defaults.js in the agent distribution for a more complete
- * description of configuration variables and their potential values.
- */
-exports.config = {
-  /**
-   * Array of application names.
-   */
+
+
+
+exports.config = {  
+  rules: { ignore : ['^/socket.io/\*/xhr-polling'] },
   app_name : ['Sails Server Ubitaxi Vzla'],
-  /**
-   * Your New Relic license key.
-   */
   license_key : 'bd28b93af43123b822f6bd602678c95695da590f',
-  logging : {
-    /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
-     */
-    level : 'info'
-  }
+  logging : { level : 'info' }
 };
