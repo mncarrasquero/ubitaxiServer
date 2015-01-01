4044ba81313d0843fa8da569c1101bed941497a8
Mario Carrasquero <mncarrasquero@gmail.com>
2015-01-01 18:15:08 +0000

    newrelic

--- app.js
+++ app.js
@@ -22,8 +22,9 @@
 (function() {
 
   var sails;
-    
+    var  newrelic;  
   try {
+    newrelic = require('newrelic');
     sails = require('sails');
   } catch (e) {
     console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
